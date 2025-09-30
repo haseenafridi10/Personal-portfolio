67df61f318f81ca8ad40ad97e31f05e29698b22d
Haseen Afridi <haseenafridi10@gmail.com>
2025-09-30 12:39:28 +0500

    fix: navbar UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
     <div className='container pt-8'>
-        <div className="flex justify-between items-center">
-            <div className="text-xl font-medium">Haseen Afridi</div>
+        <div className="flex justify-between items-center bg-gradient-to-tr from-slate-700 to-slate-500 text-white sm:p-5 p-2 sm:rounded-full rounded-lg">
+            <div className="sm:text-xl font-medium">Haseen Afridi</div>
             <ul className="gap-10 lg:gap-16 hidden md:flex">
                 <li className='menuLink'><a href="#hero">Home</a></li>
                 <li className='menuLink'><a href="#about">About</a></li>
@@ -13,8 +14,17 @@ const Navbar = () => {
                 <li className='menuLink'><a href="#skills">Skills</a></li>
                 <li className='menuLink'><a href="#contact">Contact</a></li>
             </ul>
-            <AiOutlineMenu className='md:hidden' size={30} />
+            <AiOutlineMenu onClick={() => setIsOpen(!isOpen)} className='md:hidden' size={25} />
         </div>
+        {isOpen && (
+            <ul className="flex flex-col gap-5 mt-4 md:hidden bg-gradient-to-tr from-slate-700 to-slate-500 text-white p-5 rounded-lg">
+                <li className='menuLink'><a href="#hero">Home</a></li>
+                <li className='menuLink'><a href="#about">About</a></li>
+                <li className='menuLink'><a href="#projects">Projects</a></li>
+                <li className='menuLink'><a href="#skills">Skills</a></li>
+                <li className='menuLink'><a href="#contact">Contact</a></li>
+            </ul>
+        )}
       
     </div>
   )
